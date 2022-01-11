e56c599ee675277f1a8755e0ba3215427b26ec0f
ThreshMain <martinkos007@gmail.com>
2022-01-11 10:38:39 +0100

    Added tmux plugin manager installation steps

--- install.sh
+++ install.sh
@@ -10,3 +10,9 @@ git clone https://github.com/ptavares/zsh-z.git ~/.oh-my-zsh/custom/plugins/zsh-
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/alias-tips
+
+# tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# tmux source ~/.tmux.conf
+# tmux
+# prefix + I (capital i)
