cdd5e049cff0318bb26d4cbfc7f7206d97f469de
ThreshMain <martinkos007@gmail.com>
2022-10-25 15:42:16 +0200

    Changed vim-vundle to vim-plug

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PluginInstall +qall
 
 # zsh
