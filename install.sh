1dd11b5fbfeee04a97d3cc81c8e39b69d79a3ff5
dev-null-undefined <martinkos007@gmail.com>
2022-12-19 13:10:52 +0100

    removed absolute plugins replaced with nix buildins

--- install.sh
+++ install.sh
@@ -13,8 +13,6 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/djui/alias-tips.git ${ZSH_CUSTOM:-$ZSH/custom}/plugins/alias-tips
 git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search
-git clone https://github.com/chisui/zsh-nix-shell.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/nix-shell
-git clone https://github.com/spwhitt/nix-zsh-completions.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/nix-zsh-completions
 
 # tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
